b03c9fbc409e9e932374489760543a859bd96e80
Adam <wangadam019@gmail.com>
2023-03-04 08:38:08 -0500

    get data

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 import { Container, Row, Col } from 'reactstrap';
+import { useState, useEffect } from 'react';
 
 
 
 const HeaderBanner = () => {
+
+    const [data, setData] = useState(); 
 
     function displayResults()
     {
@@ -16,14 +19,20 @@ const HeaderBanner = () => {
         } catch{
             alert('Enter More Precise Location');
         }
+        
     }
 
     function displayResults_aq(ap)
     {
         console.log(ap.list[0].components.o3)
+        setData(ap)
     }
 
-    displayResults();
+    useEffect( () => {
+        displayResults();
+    }, [])
+
+    console.log(data && data.list[0].main.aqi);
 
     return (
         <div className="static-slider-head">
@@ -31,17 +40,26 @@ const HeaderBanner = () => {
                 <Row className="justify-content-center">
                     <Col lg="8" md="6" className="align-self-center text-center">
                         <input type="text" placeholder="Enter a Location..." class="location-search-box" />
+                        <div>
+                            {data && (
+                                <ul>
+                                <li>Lon: {data.coord.lon}</li>
+                                <li>Lat: {data.coord.lat}</li>
+                                <li>O3: {data.list[0].main.aqi}</li>
+                                {/* add more list items to display other properties as needed */}
+                                </ul>
+                            )}
+                        </div>
                         <div class="location">
                         <div class="city">Location</div>
                         <div class="date">Current Date</div>
                     </div>
                     <div class="coordinates">
                         <div class="lat">Lat.</div>
-                        <span id="lat-value">TBD</span>
+                        <span id="lat-value">a</span>
                         <div class="lon">Long.</div>
                         <span id="lon-value">TBD</span>
                     </div>
-                    
                     </Col>
                 </Row>
             </Container>
